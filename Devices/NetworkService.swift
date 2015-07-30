3d6f588e5ff9ea1568f70b3b63ff984d30a3dfb5
Jon Reynolds <jon.reynolds456@gmail.com>
2015-07-30 11:48:14 -0500

    waiting for pluddy

--- Devices/NetworkService.swift
+++ Devices/NetworkService.swift
@@ -56,11 +56,93 @@ class NetworkService {
     }
     
     class func getDevice(udid: String) {
-        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let getEndpoint = NetworkService.baseURL + udid + ".json"
+            var urlRequest = NSURLRequest(URL: NSURL(string: getEndpoint)!)
+            var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+            var error = NSErrorPointer()
+            
+            var data = NSURLConnection.sendSynchronousRequest(urlRequest, returningResponse: response, error: nil)
+            if let json: NSDictionary = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary {
+                println("Synchronous reponse: \(json)")
+                
+                var user: User?
+                if let userData = json["user"] as? NSDictionary {
+                    let id = userData["id"] as? String
+                    let firstName = userData["firstName"] as? String
+                    let lastName = userData["lastName"] as? String
+                    if (id != nil && firstName != nil && lastName != nil) {
+                        user = User(id: id!, firstName: firstName!, lastName: lastName!)
+                    }
+                }
+                
+                let udid = json["udid"] as? String
+                let checkedOut = json["checkedOut"] as? Bool
+                
+                if (udid != nil && checkedOut != nil && user != nil) {
+                    let device = Device()
+                    device.setUser(user!)
+                    device.setStatus(checkedOut! ? Checked.Out : Checked.In)
+                    // set user
+                }
+                else {
+                    println("aww man, no dictionary values today.")
+                }
+                
+//                let info = [NotifUserInfoPayload: devic] as [NSObject : AnyObject]
+//                NSNotificationCenter.defaultCenter().postNotificationName(NotifGetUsersFromNetworkDidComplete, object: nil, userInfo: info)
+            }
+            else {
+                println("aww man, no json today.")
+            }
+        }
     }
     
-    class func registerDevice(udid: String) {
-        
+    class func registerDevice() {
+        // set the user on device
     }
+    
+//    class func updateDeviceStatus(device: Device, checkIn: Bool) {
+//        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+//            let getEndpoint = NetworkService.baseURL +  + ".json"
+//            var urlRequest = NSURLRequest(URL: NSURL(string: getEndpoint)!)
+//            var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+//            var error = NSErrorPointer()
+//            
+//            var data = NSURLConnection.sendSynchronousRequest(urlRequest, returningResponse: response, error: nil)
+//            if let json: NSDictionary = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary {
+//                println("Synchronous reponse: \(json)")
+//                
+//                var user: User?
+//                if let userData = json["user"] as? NSDictionary {
+//                    let id = userData["id"] as? String
+//                    let firstName = userData["firstName"] as? String
+//                    let lastName = userData["lastName"] as? String
+//                    if (id != nil && firstName != nil && lastName != nil) {
+//                        user = User(id: id!, firstName: firstName!, lastName: lastName!)
+//                    }
+//                }
+//                
+//                let udid = json["udid"] as? String
+//                let checkedOut = json["checkedOut"] as? Bool
+//                
+//                if (udid != nil && checkedOut != nil && user != nil) {
+//                    let device = Device()
+//                    device.setUser(user!)
+//                    device.setStatus(checkedOut! ? Checked.Out : Checked.In)
+//                    // set user
+//                }
+//                else {
+//                    println("aww man, no dictionary values today.")
+//                }
+//                
+//                //                let info = [NotifUserInfoPayload: devic] as [NSObject : AnyObject]
+//                //                NSNotificationCenter.defaultCenter().postNotificationName(NotifGetUsersFromNetworkDidComplete, object: nil, userInfo: info)
+//            }
+//            else {
+//                println("aww man, no json today.")
+//            }
+//        }
+//    }
     
 }
